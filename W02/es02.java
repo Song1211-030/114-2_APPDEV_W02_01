1eb39d0fc49b57a5bab28be7508f5df059f33dbb
Song1211-030 <C112181118@nkust.edu.tw>
2026-03-05 11:20:17 +0800

    first push

---
+++ W02/es02.java
@@ -0,0 +1,10 @@
+public class es02 {
+    public static void main(String[] args) {
+        int grade = 80;
+        char type = 'm';
+        if (grade > 60 && type == 'm') {
+            System.out.println("Course:" + type + "\nyou can pass:" + grade);
+        } else
+            System.out.println("課程不正確或成績不及格");
+    }
+}
